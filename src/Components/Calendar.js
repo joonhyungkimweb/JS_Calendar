95f2280d3c044a2c6e02d42c5e70c3efe66ab23f
unknown <asy8000@naver.com>
2021-07-01 20:14:58 +0900

    [feat] state에 component 추가 setState로 변경된 component만 render

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,21 +1,30 @@
 import CalendarDatesView from "./CalendarDatesView.js";
 
 export default class Calendar {
+  state = {
+    today: {
+      value: new Date(),
+      components: []
+    },
+  };
+
   constructor({ $calendar, options }) {
     this.$calendar = $calendar;
-
-    this.state = {
-      today: {
-        value: new Date(),
-      },
-    };
 
     this.datesView = new CalendarDatesView({
       $calendar,
       initialState: this.getState("today"),
     });
 
-    this.render();
+
+    this.bindComponentToState('today', this.datesView);
+
+    this.setState("today", new Date());
+
+  }
+
+  bindComponentToState(stateKey, component) {
+    this.state[stateKey].components.push(component);
   }
 
   getState(key) {
@@ -23,10 +32,8 @@ export default class Calendar {
   }
 
   setState(key, newState) {
-    this.state[key] = newState;
+    this.state[key].value = newState;
+    this.state[key].components.forEach((component) => component.setState(this.getState(key)));
   }
 
-  render() {
-    this.datesView.render();
-  }
 }
