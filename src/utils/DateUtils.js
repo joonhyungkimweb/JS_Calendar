f91273ad2c4ff17e0e0f0177a2be1b549c01f288
JHKIM <asy8000@naver.com>
2021-07-02 04:34:23 +0000

    [feat] 요일 이름 상수 배열 추가

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -1,5 +1,7 @@
 export const DAY_IN_MILLISECONDS = 8.64e+7;
 
+export const DAYS_NAME = ["SUN","MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
 export const getFirstDayOfWeek = (date) => new Date(date.getFullYear(), date.getMonth(), (date.getDate() - date.getDay()));
 
 export const getFirstDayOfMonth = (date) => new Date(date.getFullYear(), date.getMonth(), 1);
