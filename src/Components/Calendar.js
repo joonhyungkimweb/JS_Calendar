7343b2c14f818bb228d921ef846faf056877482f
JHKIM <asy8000@naver.com>
2021-07-07 03:11:17 +0000

    [feat] state 삭제

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -4,52 +4,55 @@ import CalendarHeaderView from './CalendarHeaderView';
 import CalendarDatesView from './CalendarDatesView';
 
 /* global HTMLElement */
-export default class Calendar extends HTMLElement{
+export default class Calendar extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    
-    this.state = {
-      date: {
-        value: new Date(),
-        components: [],
-      },
-    };
 
-    this.headerView = new CalendarHeaderView({
-      onPrevMonth: () => {
-        this.setState('date', getPrevMonth(this.getState('date')));
-      },
-      onNextMonth: () => {
-        this.setState('date', getNextMonth(this.getState('date')));
-      },
-    });
-    
-    
-    this.bindComponentToState('date', this.headerView)
-
-    this.datesView = new CalendarDatesView();
-    
-    this.bindComponentToState('date', this.datesView)
-    
-    this.setState('date', new Date())
-    
-    this.shadowRoot.appendChild(this.headerView);
-
-    this.shadowRoot.appendChild(this.datesView);
-
+    this.components = [
+      new CalendarHeaderView({
+        onPrevMonth: () => {
+          this.date = getPrevMonth(this.date)
+        },
+        onNextMonth: () => {
+          this.date = getNextMonth(this.date)
+        },
+      }),
+      new CalendarDatesView()
+    ]
   }
   
+  connectedCallback() {
+    this.date = new Date();
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['date'];
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    this.components.forEach(component => {
+      if(component[attrName] != null){
+        component[attrName] = newVal;
+      }
+    })
+  }
+
+  set date(newDate) {
+    this.setAttribute('date', newDate);
+  }
+
+  get date() {
+    return new Date(this.getAttribute('date'));
+  }
+
   bindComponentToState(stateKey, component) {
     this.state[stateKey].components.push(component);
   }
-
-  getState(key) {
-    return this.state[key].value;
+  
+  render(){
+    this.components.forEach(component => this.shadowRoot.appendChild(component));
   }
 
-  setState(key, newState) {
-    this.state[key].value = newState;
-    this.state[key].components.forEach((component) => component[key] = this.getState(key));
-  }
 }
